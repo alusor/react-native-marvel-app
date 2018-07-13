e5bb2981fd4fcaba74698d7b383063d89a3d24d3
Eduardo V <eddvelez@outlook.com>
2018-07-12 19:54:45 -0500

    Added firebase native ive sdk to ios using cocoapods

---
+++ ios/Pods/Target Support Files/Pods-platziApp/Pods-platziApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_platziApp : NSObject
+@end
+@implementation PodsDummy_Pods_platziApp
+@end
--- ios/platziApp/AppDelegate.m
+++ ios/platziApp/AppDelegate.m
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+#import <Firebase.h>
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
@@ -14,6 +14,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
