6f170bb0253a22d1f17088c3685bb4e14c6aed34
Eduardo V <eddvelez@outlook.com>
2018-07-13 16:31:22 -0500

    Method to fetch a large profile picture from facebook

--- src/Sagas/Session.js
+++ src/Sagas/Session.js
@@ -1,8 +1,9 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import session from '../Actions/Session';
 import { NavigationActions } from 'react-navigation';
-import { AccessToken, LoginManager } from 'react-native-fbsdk';
+import { AccessToken, LoginManager, GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
 import firebase from 'react-native-firebase';
+import axios from 'axios';
 
 async function facebookLogin() {
     try {
@@ -13,10 +14,10 @@ async function facebookLogin() {
       }
   
       console.log(`Login success with permissions: ${result.grantedPermissions.toString()}`);
-  
+      
       // get the access token
       const data = await AccessToken.getCurrentAccessToken();
-  
+      
       if (!data) {
         throw new Error('Something went wrong obtaining the users access token');
       }
@@ -28,17 +29,49 @@ async function facebookLogin() {
       const currentUser = await firebase.auth().signInAndRetrieveDataWithCredential(credential);
   
       console.info(JSON.stringify(currentUser.user.toJSON()))
+      
       return currentUser;
     } catch (e) {
       console.error(e);
       return null;
     }
   }
-
+function graphRequest() {
+  return new Promise((async (resolve, reject) => {
+    const data = await AccessToken.getCurrentAccessToken();
+      
+      const graphRequest = new GraphRequest('/me', {
+        accessToken: data.accessToken,
+        parameters: {
+          fields: {
+            string: 'picture.type(large)',
+          },
+        },
+      }, (error, result) => {
+        if (error) {
+          console.error(error)
+          reject(error);
+        } else {
+          console.log(result.picture.data.url);
+          resolve( result.picture.data.url);
+        }
+      })
+    
+      new GraphRequestManager().addRequest(graphRequest).start()
+  }))
+}
+async function getImage(){
+  const response = await graphRequest();
+  return response;
+}
 function * userLogin() {
     console.log('login saga');
     const user = yield call(facebookLogin);
+    console.log(user.user._user.photoURL);
     if(user) {
+        const image = yield call(getImage);
+        //console.log(image);
+        user.getImage = image;
         yield put(session.creators.userLoginComplete(user));
     }
         yield put(session.creators.userLoginFailed())
--- src/Views/Account.js
+++ src/Views/Account.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import Container from '../Components/Container'
+import { connect } from 'react-redux';
 
-const View = styled.View``;
+const Text = styled.Text`
+    color: white;
+    font-family: "Poppins-Bold";
+    font-size: 20px;
+    text-align: center;
+`;
 
-export default () => (
-    <View/>
-);
+const Profile = styled.Image`
+    height: 128;
+    width: 128; align-self: center;
+    border-radius: 64px;
+    margin: 30px;
+`;  
+
+const Account = (props) => {
+    const { _user } = props.user.user; 
+    return (
+    <Container>
+        <Profile source={{ uri: props.user.getImage }}/>
+        <Text>{_user.displayName}</Text>
+    </Container>
+)};
+const mapStateToProps = state => ({
+    ...state.Session
+});
+
+export default connect(mapStateToProps)(Account);
